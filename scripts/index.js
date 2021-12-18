34de0968dedf717acd5fba1bbb6ca8dc9d8bb3a7
NitzanSmulevici <nitzanye@gmail.com>
2021-12-19 01:20:24 +0200

    fix 1280 html

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,24 @@
-console.log("Hollo world");
+const settingsButton = document.querySelector(".button__info");
+const popup = document.querySelector(".popup");
+const closeButton = document.querySelector(".popup__close-button");
+const profileName = document.querySelector(".profile__name");
+const nameInput = document.querySelector(".popup__name-input");
+const profileJob = document.querySelector(".profile__job");
+const jobInput = document.querySelector(".popup__job-input");
+const form = document.querySelector(".popup__form");
+
+function togglePopup() {
+  popup.classList.toggle("popup_opened");
+  /*
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent; */
+}
+
+settingsButton.addEventListener("click", togglePopup);
+closeButton.addEventListener("click", togglePopup);
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+});
