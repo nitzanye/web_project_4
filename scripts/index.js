8226f945b09602dc4f5b96b485b0176552eee82e
NitzanSmulevici <nitzanye@gmail.com>
2022-02-27 21:08:38 +0200

    fix refactoring

--- scripts/index.js
+++ scripts/index.js
@@ -1,67 +1,22 @@
 import { Card } from "./Card.js";
 import { FormValidator } from "./FormValidator.js";
-// import * as utils from "./utils.js";
 import { PopupWithImage } from "./PopupWithImage.js";
 import { PopupWithForm } from "./PopupWithForm.js";
 import { UserInfo } from "./UserInfo.js";
 import { Section } from "./Section.js";
+import {
+  initialCards, 
+  editForm,
+  addCardForm,
+  nameInput,
+  jobInput,
+  profileEditButton,
+  addCardButton,
+  settings
+  
+}
+ from "../utils/constants.js";
 
-export const settings = {
-  formSelector: ".popup__form",
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".button_type_submit",
-  inactiveButtonClass: "button_type_submit-disabled",
-  inputErrorClass: "popup__input_type_error",
-  errorClass: "popup__error_visible",
-};
-
-// forms
-const editForm = document.querySelector(".popup__form_type_profile");
-const addCardForm = document.querySelector(".popup__form_type_add-card");
-
-// Popups
-
-const nameInput = document.querySelector(".popup__input_description_name");
-const jobInput = document.querySelector(".popup__input_description_job");
-
-
-//Buttons
-const profileEditButton = document.querySelector(".button_type_edit");
-const addCardButton = document.querySelector(".button_type_add");
-
-
-
-
-const initialCards = [
-  {
-    title: "Yosemite Valley",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
-  },
-  {
-    title: "Lake Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
-  },
-  {
-    title: "Bald Mountains",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
-  },
-  {
-    title: "Latemar",
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
-  },
-  {
-    title: "Vanoise National Park",
-    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
-  },
-  {
-    title: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg",
-  },
-].reverse();
-
-//////////////////
-//   Functions
-/////////////////
 
 
 const imagePopup = new PopupWithImage(".popup_type_preview");
@@ -86,6 +41,7 @@ const generateCard = (data) => {
   );
 };
 
+
 const section = new Section(
   {
     items: initialCards,
@@ -96,7 +52,6 @@ const section = new Section(
   },
   ".cards__list"
 );
-
 section.render();
 
 
@@ -105,10 +60,11 @@ const userInfo = new UserInfo({
   profileJobSelector: ".profile__job",
 });
 
+
 const editModal = new PopupWithForm(".popup_type_profile", (data) => {
   userInfo.setUserInfo(data);
 });
-editModal.setEventListeners();
+
 
 const addCardModal = new PopupWithForm(
   ".popup_type_add-card",
@@ -119,15 +75,19 @@ const addCardModal = new PopupWithForm(
   }
 );
 
+
+editModal.setEventListeners();
 addCardModal.setEventListeners();
+imagePopup.setEventListeners();
+
 
 const editFormValidator = new FormValidator(settings, editForm);
 const addCardFormValidator = new FormValidator(settings, addCardForm);
 
-imagePopup.setEventListeners();
 
 editFormValidator.enableValidation();
 addCardFormValidator.enableValidation();
+
 
 addCardButton.addEventListener("click", () => {
   addCardFormValidator.resetValidation();
---
+++ utils/constants.js
@@ -0,0 +1,49 @@
+// forms
+export const editForm = document.querySelector(".popup__form_type_profile");
+export const addCardForm = document.querySelector(".popup__form_type_add-card");
+
+// Popups
+export const nameInput = document.querySelector(".popup__input_description_name");
+export const jobInput = document.querySelector(".popup__input_description_job");
+
+
+//Buttons
+export const profileEditButton = document.querySelector(".button_type_edit");
+export const addCardButton = document.querySelector(".button_type_add");
+
+
+export const settings = {
+    formSelector: ".popup__form",
+    inputSelector: ".popup__input",
+    submitButtonSelector: ".button_type_submit",
+    inactiveButtonClass: "button_type_submit-disabled",
+    inputErrorClass: "popup__input_type_error",
+    errorClass: "popup__error_visible",
+  };
+
+export const initialCards = [
+    {
+      title: "Yosemite Valley",
+      link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+    },
+    {
+      title: "Lake Louise",
+      link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+    },
+    {
+      title: "Bald Mountains",
+      link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+    },
+    {
+      title: "Latemar",
+      link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+    },
+    {
+      title: "Vanoise National Park",
+      link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+    },
+    {
+      title: "Lago di Braies",
+      link: "https://code.s3.yandex.net/web-code/lago.jpg",
+    },
+  ].reverse();
