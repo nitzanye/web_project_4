c9fe83689488995f81cb3eef7030afe207f226eb
NitzanSmulevici <nitzanye@gmail.com>
2022-03-01 22:59:16 +0200

    fix comments

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,8 +1,6 @@
-
 export class Popup {
   constructor(popupSelector) {
     this._popupElement = document.querySelector(popupSelector);
-    this.close = this.close.bind(this)
   }
 
   _handleEscClose = (evt) => {
@@ -17,7 +15,8 @@ export class Popup {
     this._popupElement.addEventListener("click", (evt) => {
       if (
         evt.target.classList.contains("popup") ||
-        evt.target.classList.contains("button_type_close")) {
+        evt.target.classList.contains("button_type_close")
+      ) {
         this.close();
       }
     });
@@ -26,13 +25,10 @@ export class Popup {
   open() {
     this._popupElement.classList.add("popup_opened");
     document.addEventListener("keyup", this._handleEscClose);
-    
   }
 
   close() {
     this._popupElement.classList.remove("popup_opened");
     document.removeEventListener("keyup", this._handleEscClose);
-  };
+  }
 }
-
-
--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -2,16 +2,14 @@ import { Popup } from "./Popup.js"; // once we work with webpack we don't need j
 
 export class PopupWithImage extends Popup {
   
-    constructor(popupSelector) {
-        super(popupSelector);
-    }
-
-
+   
     open(title, link) { 
-        super.open();
+        
         this._popupElement.querySelector(".popup__caption").textContent = title;
         this._popupElement.querySelector(".popup__preview-image").src = link;
         this._popupElement.querySelector(".popup__preview-image").alt = title;
+
+        super.open();
   }
 }
 
